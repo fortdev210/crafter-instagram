86df1c33d63e6dcf3f687a5a314603ff5642e284
fortdev210 <sharp.blader2050@gmail.com>
2023-04-18 16:55:35 +0800

    show posts of users

--- crafter/urls.py
+++ crafter/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path 
-from .views import ConnectInstagram
+from django.views.generic import TemplateView
 
-app_name = 'instagram'
+app_name = 'crafter'
 
 urlpatterns = [
-    path('', ConnectInstagram.as_view(), name='connect_instagram')
+   path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
--- crafter/views.py
+++ crafter/views.py
@@ -1,13 +1,31 @@
-from django.views.generic import TemplateView
+import requests
 from allauth.socialaccount.models import SocialAccount
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 
-class ConnectInstagram(TemplateView):
-  template_name = 'connect.html'
-# def connect_instagram(request):
-#   # instagram_account = SocialAccount.objects.filter(user=request.user, provider='instagram').first()
-#   # context = {
-#   #   'instagram_account': instagram_account
-#   # }
-#   return render(request, 'crafter/connect.html')
-  
+@login_required
+def home(request):
+    user = request.user
+    try:
+        account = SocialAccount.objects.get(user=user, provider='instagram')
+    except SocialAccount.DoesNotExist:
+        account = None
+
+    posts = []
+    if account:
+        access_token = account.socialtoken_set.first().token
+        response = requests.get(
+            f'https://graph.instagram.com/me/media?fields=id,caption,media_url,thumbnail_url,permalink,timestamp,username&access_token={access_token}'
+        )
+        if response.ok:
+            data = response.json()
+            for post in data['data']:
+                posts.append({
+                    'id': post['id'],
+                    'caption': post['caption'],
+                    'image_url': post['media_url'],
+                    'likes': 0,  # You can fetch the number of likes using the Instagram Graph API
+                })
+
+    return render(request, 'home.html', {'user': user, 'posts': posts})
