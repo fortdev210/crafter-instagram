ae9a3425001de8fb59a4a984f56971b3719f0a16
fortdev210 <sharp.blader2050@gmail.com>
2023-04-19 13:52:54 +0800

    update models

--- crafter/models.py
+++ crafter/models.py
@@ -1,14 +1,28 @@
-from django.db import models
+from django.db import IntegrityError, models
 
 
 class InstagramPost(models.Model):
     user_id = models.CharField(max_length=255)
-    post_id = models.CharField(max_length=255)
+    post_id = models.CharField(max_length=255, unique=True)
     caption = models.TextField()
     media_type = models.CharField(max_length=255)
     media_url = models.TextField()
-    
+
     @classmethod
     def get_posts_by_user(cls, user_id):
         return cls.objects.filter(user_id=user_id).values()
-        
+
+    @classmethod
+    def save_post(cls, post_data):
+        try:
+            instagram_post = cls(
+                user_id=post_data['user_id'],
+                post_id=post_data['post_id'],
+                caption=post_data['caption'],
+                media_type=post_data['media_type'],
+                media_url=post_data['media_url']
+            )
+            instagram_post.save()
+        except IntegrityError:
+            # Handle if the post with the same post_id already exists
+            pass
--- crafter/views.py
+++ crafter/views.py
@@ -11,6 +11,7 @@ def connect_instagram(request):
     oauth_url = f"https://api.instagram.com/oauth/authorize?client_id={settings.INSTAGRAM_APP_ID}&scope=user_profile,user_media&response_type=code&redirect_uri={settings.INSTAGRAM_REDIRECT_URI}"
     return render(request, 'connect.html', {'url': oauth_url})
 
+
 def exchange_code_token(request):
     code = request.GET.get('code')
     url = 'https://api.instagram.com/oauth/access_token'
@@ -25,13 +26,11 @@ def exchange_code_token(request):
         'code': code
     }
     response = requests.post(url, headers=headers, data=data)
-    
+
     if response.ok:
         data = response.json()
         user_id = data['user_id']
         access_token = data['access_token']
-        print("user id ", user_id)
-        print("access token ", access_token)
         api_url = f'https://graph.instagram.com/{user_id}/media'
 
         # Set the API parameters
@@ -46,25 +45,20 @@ def exchange_code_token(request):
 
         # Parse the JSON response
         json_response = response.json()
-       
+
         # Extract the list of posts
         posts = json_response['data']
 
         # Loop through the posts and print their captions and permalinks
         for post in posts:
             # Create an InstagramPost object with the retrieved data
-            InstagramPost.objects.create(
-                user_id=user_id,
-                post_id=post['id'],
-                caption=post['caption'],
-                media_type=post['media_type'],
-                media_url=post['media_url'],
-            )
+            InstagramPost.save_post(post)
         return redirect(reverse('crafter:show_posts') + f'?user_id={user_id}')
     return redirect(reverse('crafter:connect_instagram'))
+
 
 def show_posts(request):
     user_id = request.GET.get('user_id')
     posts = InstagramPost.get_posts_by_user(user_id)
 
-    return render(request, 'posts.html', {'posts': posts}) 
+    return render(request, 'posts.html', {'posts': posts})
